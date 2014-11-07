a9cff024fb61e7a2c6ef2a1e2d062cb4523befd7
suneetsasidharan <suneet.sasidharan@gmail.com>
2014-11-07 02:52:02 -0800

    Full Changes made

--- files/js/main.js
+++ files/js/main.js
@@ -99,7 +99,7 @@ $('.chart').waypoint(function() {
 				{ src:'img/slider/02.jpg', fade:1000 },
 				{ src:'img/slider/03.jpg', fade:1000 },
 				{ src:'img/slider/04.jpg', fade:1000 }
-			  ]
+              ]
 			})('overlay', {
 			  src:'img/overlays/16.png'
 			});
